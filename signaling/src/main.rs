1558d2e11f12431b381d4a495a97ce4791721ee6
William Quintal <william95quintalwilliam@outlook.com>
2024-12-28 14:41:42 -0500

    signaling change to exemple from matchbox repo

--- signaling/src/main.rs
+++ signaling/src/main.rs
@@ -1,37 +1,72 @@
+mod args;
+mod state;
+mod topology;
 
-use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*, utils::Duration};
-use bevy_matchbox::{matchbox_signaling::SignalingServer, prelude::*};
-use std::net::{Ipv4Addr, SocketAddrV4};
+use crate::{
+    state::{RequestedRoom, RoomId, ServerState},
+    topology::MatchmakingDemoTopology,
+};
+use args::Args;
+use axum::{http::StatusCode, response::IntoResponse, routing::get};
+use clap::Parser;
+use matchbox_signaling::SignalingServerBuilder;
+use tracing::info;
+use tracing_subscriber::prelude::*;
 
-fn main() {
-    App::new()
-        .add_plugins((
-            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f32(
-                1. / 120., // be nice to the CPU
-            ))),
-            LogPlugin::default(),
-        ))
-        .add_systems(Startup, (start_signaling_server).chain())
-        .run();
+fn setup_logging() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "matchbox_server=info,tower_http=debug".into()),
+        )
+        .with(
+            tracing_subscriber::fmt::layer()
+                .compact()
+                .with_file(false)
+                .with_target(false),
+        )
+        .init();
 }
 
-fn start_signaling_server(mut commands: Commands) {
-    info!("Starting signaling server");
-    let addr = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 3536);
-    let signaling_server = MatchboxServer::from(
-        SignalingServer::client_server_builder(addr)
-            .on_connection_request(|connection| {
-                info!("Connecting: {connection:?}");
-                Ok(true) // Allow all connections
-            })
-            .on_id_assignment(|(socket, id)| info!("{socket} received {id}"))
-            .on_host_connected(|id| info!("Host joined: {id}"))
-            .on_host_disconnected(|id| info!("Host left: {id}"))
-            .on_client_connected(|id| info!("Client joined: {id}"))
-            .on_client_disconnected(|id| info!("Client left: {id}"))
-            .cors()
-            .trace()
-            .build(),
-    );
-    commands.insert_resource(signaling_server);
+#[tokio::main]
+async fn main() {
+    setup_logging();
+    let args = Args::parse();
+
+    // Setup router
+    info!("Matchbox Signaling Server: {}", args.host);
+
+    let mut state = ServerState::default();
+    let server = SignalingServerBuilder::new(args.host, MatchmakingDemoTopology, state.clone())
+        .on_connection_request({
+            let mut state = state.clone();
+            move |connection| {
+                let room_id = RoomId(connection.path.clone().unwrap_or_default());
+                let next = connection
+                    .query_params
+                    .get("next")
+                    .and_then(|next| next.parse::<usize>().ok());
+                let room = RequestedRoom { id: room_id, next };
+                state.add_waiting_client(connection.origin, room);
+                Ok(true) // allow all clients
+            }
+        })
+        .on_id_assignment({
+            move |(origin, peer_id)| {
+                info!("Client connected {origin:?}: {peer_id:?}");
+                state.assign_id_to_waiting_client(origin, peer_id);
+            }
+        })
+        .cors()
+        .trace()
+        .mutate_router(|router| router.route("/health", get(health_handler)))
+        .build();
+    server
+        .serve()
+        .await
+        .expect("Unable to run signaling server, is it already running?")
+}
+
+pub async fn health_handler() -> impl IntoResponse {
+    StatusCode::OK
 }
